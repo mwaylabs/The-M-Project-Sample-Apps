347f31fd850e37f808a8ad84a16a0cf235aca9d0
Dominik <d.laubach@mwaysolutions.com>
2011-01-19 17:10:12 +0100

    Added dualbind sample app

---
+++ DualbindSample/app/controllers/InputController.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 0.0.6
+//
+// Project: DualbindSample
+// Controller: InputController
+// ==========================================================================
+
+DualbindSample.InputController = M.Controller.extend({
+
+    input: null
+
+});
---
+++ DualbindSample/app/main.js
@@ -0,0 +1,58 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 0.0.6
+//
+// Project: DualbindSample 
+// ==========================================================================
+
+var DualbindSample  = DualbindSample || {};
+
+DualbindSample.app = M.Application.design({
+
+    page: M.PageView.design({
+
+        childViews: 'header content',
+
+        header: M.ToolbarView.design({
+                value: 'Dualbind Sample',
+                anchorLocation : M.TOP
+        }),
+
+        content: M.ScrollView.design({
+
+            /* order in childViews string defines render order*/
+            childViews: 'inputField1 inputField2 inputField3 inputField4',
+
+            inputField1: M.TextFieldView.design({
+                name: 'input_field1',
+                contentBinding: 'DualbindSample.InputController.input',
+                contentBindingReverse: 'DualbindSample.InputController.input',
+                initialText: 'Enter some text here ...'
+            }),
+
+            inputField2: M.TextFieldView.design({
+                name: 'input_field2',
+                contentBinding: 'DualbindSample.InputController.input',
+                contentBindingReverse: 'DualbindSample.InputController.input',
+                initialText: '... or here ...'
+            }),
+
+            inputField3: M.TextFieldView.design({
+                name: 'input_field3',
+                contentBinding: 'DualbindSample.InputController.input',
+                contentBindingReverse: 'DualbindSample.InputController.input',
+                initialText: '... or here ...'
+            }),
+
+            inputField4: M.TextFieldView.design({
+                name: 'input_field4',
+                contentBinding: 'DualbindSample.InputController.input',
+                contentBindingReverse: 'DualbindSample.InputController.input',
+                initialText: '... or even here!'
+            })
+
+        })
+
+    })
+
+});
