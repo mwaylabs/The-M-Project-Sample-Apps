c4a9e372188765d008dbbc5b270c4e3c7d014250
Dominik <d.laubach@mwaysolutions.com>
2011-02-01 16:19:01 +0100

    Added chartin sample application as demo for including third party libraries

---
+++ ChartingSample/app/main.js
@@ -0,0 +1,48 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 0.0.6
+//
+// Project: ChartingSample 
+// ==========================================================================
+
+var ChartingSample  = ChartingSample || {};
+
+ChartingSample.app = M.Application.design({
+
+    page1: M.PageView.design({
+
+        childViews: 'header content',
+
+        onLoad: {
+            target: ChartingSample.ChartingController,
+            action: 'init'
+        },
+
+        onOrientationChange: {
+            target: ChartingSample.ChartingController,
+            action: 'resize'
+        },
+
+        header: M.ToolbarView.design({
+    
+            anchorLocation: M.TOP,
+
+            value: 'Raphaël Demo'
+
+        }),
+
+        content: M.ScrollView.design({
+
+            childViews: 'canvas',
+
+            canvas: M.ContainerView.design({
+
+                
+
+            })
+
+        })
+
+    })
+
+});
