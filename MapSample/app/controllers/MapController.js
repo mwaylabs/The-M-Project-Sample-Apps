05788791610002a5646dd08683512a7d5ade671b
Dominik <mail@dominik-laubach.de>
2011-02-03 13:44:22 +0100

    Added a map sample application

---
+++ MapSample/app/controllers/MapController.js
@@ -0,0 +1,140 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 0.0.7-1
+//
+// Project: MapSample
+// Controller: MapController
+// ==========================================================================
+
+MapSample.MapController = M.Controller.extend({
+
+    markers: null,
+
+    textFieldValue: null,
+
+    init: function(isFirstTime) {
+        if(isFirstTime) {
+
+            M.ViewManager.getView('page', 'map').initMap({});
+            
+        }
+    },
+
+    applyDB: function() {
+        M.ViewManager.getView('page', 'map').updateMap({
+            initialLocation: M.Location.init(48.778495, 9.180951),
+            zoomLevel: 12,
+            markerAnimationType: M.MAP_MARKER_ANIMATION_DROP
+        });
+
+        var map = M.ViewManager.getView('page', 'map');
+        var locations = [
+            M.MapMarkerView.init({
+                location: M.Location.init(48.778495, 9.180951),
+                map: map,
+                title: 'Marker 1',
+                message: 'I am marker 1. Welcome to my annotation!'
+            }),
+            M.MapMarkerView.init({
+                location: M.Location.init(48.777534, 9.170566),
+                map: map,
+                title: 'Marker 2',
+                message: 'I am marker 2. Welcome to my annotation!'
+            }),
+            M.MapMarkerView.init({
+                location: M.Location.init(48.772273, 9.185328),
+                map: map,
+                title: 'Marker 3',
+                message: 'I am marker 3. Welcome to my annotation!'
+            }),
+            M.MapMarkerView.init({
+                location: M.Location.init(48.783303, 9.169793),
+                map: map,
+                title: 'Marker 4',
+                message: 'I am marker 4. Welcome to my annotation!'
+            }),
+            M.MapMarkerView.init({
+                location: M.Location.init(48.783529, 9.194856),
+                map: map,
+                title: 'Marker 5',
+                message: 'I am marker 5. Welcome to my annotation!'
+            })
+        ];
+
+        this.set('markers', locations);
+    },
+
+    findMe: function() {
+        M.LoaderView.show('Looking for you ...');
+
+        M.LocationManager.getLocation(this, this.onSuccess, this.onError, {
+            maximumAge: 0,
+            timeout: 10000,
+            accuracy: 200
+        });
+    },
+
+    onSuccess: function(location) {
+        M.LoaderView.hide();
+
+        this.location = location;
+
+        M.ViewManager.getView('page', 'map').updateMap({
+            initialLocation: location,
+            zoomLevel: 14,
+            setMarkerAtInitialLocation: YES,
+            markerAnimationType: M.MAP_MARKER_ANIMATION_DROP
+        });
+    },
+
+    onError: function(error) {
+        M.LoaderView.hide();
+
+        M.DialogView.alert({
+            title: 'Error: ' + error,
+            message: 'An error occured while trying to find your location. Please check your service and try again later.'
+        });
+    },
+
+    lookUp: function() {
+        var input = M.ViewManager.getView('page', 'textfield').value;
+
+        if(!input || input === '') {
+            M.DialogView.alert({
+                title: 'Error',
+                message: 'Please enter a valid address.'
+            });
+        } else {
+            M.LoaderView.show('Looking up your location ...');
+            M.LocationManager.getLocationByAddress(this, this.lookUpSuccess, this.lookUpError, input);
+        }
+    },
+
+    lookUpSuccess: function(location) {
+        M.LoaderView.hide();
+
+        M.ViewManager.getView('page', 'map').updateMap({
+            initialLocation: location,
+            zoomLevel: 14,
+            setMarkerAtInitialLocation: YES,
+            markerAnimationType: M.MAP_MARKER_ANIMATION_DROP,
+            isDraggable: YES
+        });
+    },
+
+    lookUpError: function(error) {
+        M.LoaderView.hide();
+
+        M.DialogView.alert({
+            title: 'Error: ' + error,
+            message: 'An error occured while trying to lookup the entered location. Please check both your service and your input, then try again.'
+        });
+    },
+
+    markerClicked: function(id, marker) {
+        if(marker) {
+            // do something with this marker...
+        }
+    }
+
+});
---
+++ MapSample/app/main.js
@@ -0,0 +1,120 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 0.0.7-1
+//
+// Project: MapSample 
+// ==========================================================================
+
+var MapSample  = MapSample || {};
+
+MapSample.app = M.Application.design({
+
+    page: M.PageView.design({
+
+        onLoad: {
+
+            target: MapSample.MapController,
+
+            action: 'init'
+
+        },
+
+        childViews: 'header content',
+
+        header: M.ToolbarView.design({
+
+            value: 'Map Sample',
+
+            anchorLocation: M.TOP
+
+        }),
+
+        content: M.ScrollView.design({
+
+            childViews: 'map grid textfield searchBtn spacer',
+
+            map: M.MapView.design({
+
+                cssClass: 'map',
+
+                isInset: YES,
+
+                showMapTypeControl: NO,
+
+                showStreetViewControl: NO,
+
+                showNavigationControl: NO,
+
+                mapType: M.MAP_ROADMAP,
+
+                zoomLevel: 10,
+
+                isDraggable: NO,
+
+                contentBinding: 'MapSample.MapController.markers',
+
+                target: MapSample.MapController,
+
+                action: 'markerClicked'
+
+            }),
+
+            grid: M.GridView.design({
+
+                childViews: 'updateBtn applyDBBtn',
+
+                layout: M.TWO_COLUMNS,
+
+                updateBtn: M.ButtonView.design({
+
+                    value: 'Find Me',
+
+                    target: MapSample.MapController,
+
+                    action: 'findMe'
+
+                }),
+
+                applyDBBtn: M.ButtonView.design({
+
+                    value: 'Data Binding',
+
+                    target: MapSample.MapController,
+
+                    action: 'applyDB'
+
+                })
+
+            }),
+
+            textfield: M.TextFieldView.design({
+
+                isGrouped: NO,
+
+                cssClass: 'textField',
+
+                contentBinding: 'MapSample.MapController.textFieldValue'
+
+            }),
+
+            searchBtn: M.ButtonView.design({
+
+                value: 'Show Address on Map',
+
+                target: MapSample.MapController,
+
+                action: 'lookUp'
+
+            }),
+
+            spacer: M.LabelView.design({
+
+                value: '&#160;'
+
+            })
+
+        })
+
+    })
+
+});
