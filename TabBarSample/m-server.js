36924fed28f880ece7b536825f49aa76b5b2b6e2
Sebastian <s.werler@mwaysolutions.com>
2011-02-22 15:56:51 +0100

    new framework

--- TabBarSample/m-server.js
+++ TabBarSample/m-server.js
@@ -2,16 +2,6 @@
 
 var espresso  = require('../../Espresso/core/espresso').Espresso;
 
-var server = new espresso.Server(true);
+var server = new espresso.Server(__dirname);
 
-var app = server.getNewApp(__dirname);
-
-app.loadTheApplication();
-
-app.loadTheMProject();
-
-app.build(function (options) {
-    app.prepareForServer(function (opt){
-        server.run("TabBarSample");
-    })
-});
+    server.run("TabBarSample");
