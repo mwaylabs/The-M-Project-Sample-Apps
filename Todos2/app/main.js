eb3a560866a0d517291c51122b0f40af33555c51
Dominik <mail@dominik-laubach.de>
2011-06-06 17:51:40 +0200

    switched selection list to dialog mode

--- Todos2/app/main.js
+++ Todos2/app/main.js
@@ -387,7 +387,7 @@ Todos2.app = M.Application.design({
 
                 childViews: 'item1 item2 item3 item4 item5 item6 item7 item8 item9 item10 item11 item12 item13',
 
-                selectionMode: M.SINGLE_SELECTION,
+                selectionMode: M.SINGLE_SELECTION_DIALOG,
 
                 label: M.I18N.l('language'),
 
