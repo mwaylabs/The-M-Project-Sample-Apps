8ad5f2db48fa7988dd89bfc02372b2af5759f920
dominiklaubach <d.laubach@panacoda.com>
2011-07-04 11:03:51 +0200

    Added VersionSample

---
+++ VersionSample/app/main.js
@@ -0,0 +1,36 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 
+//
+// Project: VersionSample 
+// ==========================================================================
+
+var VersionSample  = VersionSample || {};
+
+VersionSample.app = M.Application.design({
+
+    entryPage : 'page1',
+
+    page1: M.PageView.design({
+
+        childViews: 'header content',
+
+        header: M.ToolbarView.design({
+            value: 'Version Sample'
+        }),
+
+        content: M.ScrollView.design({
+            childViews: 'label',
+
+            label: M.LabelView.design({
+                computedValue: {
+                    operation: function(v) {
+                        return M.version
+                    }
+                }
+            })
+        })
+
+    })
+
+});
