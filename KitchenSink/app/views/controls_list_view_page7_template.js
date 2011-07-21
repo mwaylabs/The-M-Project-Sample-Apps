d5f66d34723809864047f6d5a2ebd0cf6bca87c6
dominiklaubach <d.laubach@panacoda.com>
2011-07-21 10:17:36 +0200

    added list sample for non-selectable list items

---
+++ KitchenSink/app/views/controls_list_view_page7_template.js
@@ -0,0 +1,13 @@
+KitchenSink.ControlsListViewPage7Template = M.ListItemView.design({
+
+    childViews: 'name',
+
+    isSelectable: NO,
+
+    name: M.LabelView.design({
+
+        valuePattern: '<%= name %>'
+
+    })
+
+});
