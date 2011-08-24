f784ab7cb9f3300e1813c7d6cff9991ddec7bd83
dominiklaubach <d.laubach@panacoda.com>
2011-08-24 09:13:38 +0200

    added non-broken datepicker sample

---
+++ DatepickerSample/app/controllers/ApplicationController.js
@@ -0,0 +1,112 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 
+//
+// Project: DatepickerSample
+// Controller: ApplicationController
+// ==========================================================================
+
+DatepickerSample.ApplicationController = M.Controller.extend({
+
+    getDate: function() {
+        M.DatePickerView.show({
+            source: M.ViewManager.getView('page1', 'text'),
+            useSourceDateAsInitialDate: NO,
+            showTimePicker: NO,
+            callbacks: {
+                before: {
+                    action: function(value, date) {
+                        console.log('--- START getDate ---');
+                        console.log('## before ##');
+                        if(value) {
+                            console.log('value: ' + value);
+                        }
+                    }
+                },
+                confirm: {
+                    action: function(value, date) {
+                        console.log('## confirm ##');
+                        if(value) {
+                            console.log('value: ' + value);
+                            console.log('--- END getDate ---');
+                        }
+                    }
+                },
+                cancel: {
+                    action: function() {
+                        console.log('## cancel ##');
+                        console.log('--- END getDate ---');
+                    }
+                }
+            }
+        });
+    },
+
+    getTime: function() {
+        M.DatePickerView.show({
+            source: M.ViewManager.getView('page1', 'text'),
+            useSourceDateAsInitialDate: NO,
+            showDatePicker: NO,
+            callbacks: {
+                before: {
+                    action: function(value, date) {
+                        console.log('--- START getTime ---');
+                        console.log('## before ##');
+                        if(value) {
+                            console.log('value: ' + value);
+                        }
+                    }
+                },
+                confirm: {
+                    action: function(value, date) {
+                        console.log('## confirm ##');
+                        if(value) {
+                            console.log('value: ' + value);
+                            console.log('--- END getTime ---');
+                        }
+                    }
+                },
+                cancel: {
+                    action: function() {
+                        console.log('## cancel ##');
+                        console.log('--- END getTime ---');
+                    }
+                }
+            }
+        });
+    },
+
+    getDatetime: function() {
+        M.DatePickerView.show({
+            source: M.ViewManager.getView('page1', 'text'),
+            useSourceDateAsInitialDate: NO,
+            callbacks: {
+                before: {
+                    action: function(value, date) {
+                        console.log('--- START getDatetime ---');
+                        console.log('## before ##');
+                        if(value) {
+                            console.log('value: ' + value);
+                        }
+                    }
+                },
+                confirm: {
+                    action: function(value, date) {
+                        console.log('## confirm ##');
+                        if(value) {
+                            console.log('value: ' + value);
+                            console.log('--- END getDatetime ---');
+                        }
+                    }
+                },
+                cancel: {
+                    action: function() {
+                        console.log('## cancel ##');
+                        console.log('--- END getDatetime ---');
+                    }
+                }
+            }
+        });
+    }
+
+});
