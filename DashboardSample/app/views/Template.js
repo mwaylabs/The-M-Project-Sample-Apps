4e30c78b5635dfa8b21b663dc548d0e48bc44d8a
dominiklaubach <d.laubach@panacoda.com>
2011-08-24 09:33:30 +0200

    Removed broken dashboard sample

--- DashboardSample/app/views/Template.js
+++
@@ -1,7 +0,0 @@
-DashboardSample.TemplateView = M.ListItemView.design({
-    childViews: 'string',
-    isSelectable: NO,
-    string: M.LabelView.design({
-        valuePattern: '<%= label %>'
-    })
-});
