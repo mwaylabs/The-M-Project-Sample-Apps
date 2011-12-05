63c32ab9ae2081fdf4f4925096d54cd045365266
dominiklaubach <d.laubach@panacoda.com>
2011-12-05 15:57:52 +0100

    use callbacks property of M.Request

--- Twitter/app/controllers/twitter_controller.js
+++ Twitter/app/controllers/twitter_controller.js
@@ -33,34 +33,74 @@ Twitter.TwitterController = M.Controller.extend({
             url: '/twitter/search.json?q=' + searchString + '&rpp=10',
             isJSON: YES,
             timeout: 5000,
-            beforeSend: function(req) {
-                //...
-            },
-            onSuccess: function(data){
-                M.LoaderView.hide();
-                if(data && data.results && data.results.length > 0) {
-                    Twitter.TwitterController.set('results', data);
-                    Twitter.TwitterController.set('searchString', 'Results for \'' + searchString + '\'');
-                    M.Controller.switchToPage(M.ViewManager.getPage('page2'));
-                } else if (data && data.results) {
-                   M.DialogView.alert({
-                       title: 'Nothing found...',
-                       message: 'Your search for \'' + searchString + '\' didn\'t bring up any results. Please try something else.'
-                   });
-                } else {
-                   M.DialogView.alert({
-                       title: 'Connection Error',
-                       message: 'No connection could be established! Please check your connection status and try again. If this message keeps coming up, you probably didn\'t add the necessary proxy to your server.'
-                   });
+            sendTimestamp: YES,
+            callbacks: {
+                beforeSend: {
+                    target: this,
+                    action: function() {
+                        // ...
+                    }
+                },
+                success: {
+                    target: this,
+                    action: function(data) {
+                        M.LoaderView.hide();
+                        if(data && data.results && data.results.length > 0) {
+                            this.set('results', data);
+                            this.set('searchString', 'Results for \'' + searchString + '\'');
+                            this.switchToPage(M.ViewManager.getPage('page2'));
+                        } else if (data && data.results) {
+                           M.DialogView.alert({
+                               title: 'Nothing found...',
+                               message: 'Your search for \'' + searchString + '\' didn\'t bring up any results. Please try something else.'
+                           });
+                        } else {
+                           M.DialogView.alert({
+                               title: 'Connection Error',
+                               message: 'No connection could be established! Please check your connection status and try again. If this message keeps coming up, you probably didn\'t add the necessary proxy to your server.'
+                           });
+                        }
+                    }
+                },
+                error: {
+                    target: this,
+                    action: function() {
+                        M.DialogView.alert({
+                            title: 'Request failed',
+                            message: 'The search could not be performed! Please check your network status and try again.'
+                        });
+                        M.LoaderView.hide();
+                    }
                 }
-            },
-            onError: function(data, error){
-                M.DialogView.alert({
-                    title: 'Request failed',
-                    message: 'The search could not be performed! Please check your network status and try again.'
-                });
-                M.LoaderView.hide();
             }
+//            ,beforeSend: function(req) {
+//                //...
+//            },
+//            onSuccess: function(data){
+//                M.LoaderView.hide();
+//                if(data && data.results && data.results.length > 0) {
+//                    Twitter.TwitterController.set('results', data);
+//                    Twitter.TwitterController.set('searchString', 'Results for \'' + searchString + '\'');
+//                    M.Controller.switchToPage(M.ViewManager.getPage('page2'));
+//                } else if (data && data.results) {
+//                   M.DialogView.alert({
+//                       title: 'Nothing found...',
+//                       message: 'Your search for \'' + searchString + '\' didn\'t bring up any results. Please try something else.'
+//                   });
+//                } else {
+//                   M.DialogView.alert({
+//                       title: 'Connection Error',
+//                       message: 'No connection could be established! Please check your connection status and try again. If this message keeps coming up, you probably didn\'t add the necessary proxy to your server.'
+//                   });
+//                }
+//            },
+//            onError: function(data, error){
+//                M.DialogView.alert({
+//                    title: 'Request failed',
+//                    message: 'The search could not be performed! Please check your network status and try again.'
+//                });
+//                M.LoaderView.hide();
+//            }
         }).send();
     },
 
