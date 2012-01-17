ea53adc1d454a82a17f24d3e6e18057957e38c09
dominiklaubach <d.laubach@panacoda.com>
2012-01-17 21:02:05 +0100

    removed web sql sample

--- KitchenSink/app/controllers/data_controller.js
+++ KitchenSink/app/controllers/data_controller.js
@@ -21,10 +21,10 @@ KitchenSink.DataController = M.Controller.extend({
                     name: "LocalStorage ToDo App Example",
                     page: "dataLocalStorageTaskApp"
                 },
-                {
-                    name: "WebSQL ToDo App Example",
-                    page: "dataWebSqlTaskApp"
-                },
+//                {
+//                    name: "WebSQL ToDo App Example",
+//                    page: "dataWebSqlTaskApp"
+//                },
                 {
                     name: "M.Request: Send GET Request",
                     page: "dataRequestSample"
