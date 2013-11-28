2b55c44153fff1e56be9a92618eb28b327822f09
stefanbuck <web@broud.de>
2013-11-28 11:18:39 +0100

    add option to update dependencies

--- setup-all.sh
+++ setup-all.sh
@@ -1,12 +1,30 @@
 #! /usr/bin/env sh
 
-for dir in *; do
-	test -d "$dir" || continue
-    cd $dir
-    echo "Run setup for" $dir
-    bower install
-    npm install
-    cd -
-done
+updateDependencies="y";
+nodedir=$(find . -type d -name 'node_modules' -maxdepth 2)
+if [ "$nodedir" != "" ]
+then
+	read -p "Would you like to update all bower / npm dependencies (y/N)? " updateDependencies
+	if [ "$updateDependencies" = "y" ]; then
+		for dir in *; do
+			test -d "$dir" || continue
+		    cd $dir
+		    rm -rf app/bower_components
+		    rm -rf node_modules
+		    cd -
+		done
+	fi
+fi
 
-echo "Done!"
+if [ "$updateDependencies" = "y" ]
+then
+	for dir in *; do
+		test -d "$dir" || continue
+	    cd $dir
+	    echo "Run setup for" $dir
+	    bower install
+	    npm install
+	    cd -
+	done
+fi
+
