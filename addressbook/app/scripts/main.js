426127094ab638740b1bcbddac0c6cc15c512c8f
stefanbuck <web@broud.de>
2013-12-03 17:37:30 +0100

    add addressbook sample

---
+++ addressbook/app/scripts/main.js
@@ -0,0 +1,38 @@
+/*global $*/
+
+// PLEASE DON'T CHANGE OR REMOVE THE COMMENTS.
+// All comments in this file are necessary for the build process.
+
+(function( global ) {
+
+    global.Addressbook = M.Application.extend().create(global.Addressbook.mconfig);
+
+    $(document).ready(function() {
+        'use strict';
+
+        Addressbook.start({
+            routing: {
+                routes: {
+                    '': 'indexCtrl',
+                    'detail/:id': 'detailCtrl',
+                    'edit/:id': 'editCtrl',
+                    'add': 'addCtrl',
+                    //m:routes
+                },
+
+                initialize: function() {
+                    M.Router.prototype.initialize.apply(this, arguments);
+                    Addressbook.ListController = this.indexCtrl;
+                },
+
+                indexCtrl: Addressbook.Controllers.ListController.create(),
+                detailCtrl: Addressbook.Controllers.DetailController.create(),
+                editCtrl: Addressbook.Controllers.EditController.create(),
+                addCtrl: Addressbook.Controllers.AddController.create(),
+                //m:controllers
+            }
+        });
+    });
+
+})(this);
+
